69f394af9d2484eb0374e63d51167feb6f479713
leebrouse <leebrouse7@gmail.com>
2025-03-18 17:16:40 +0800

    complete 03-3 Creating and Connecting the postgres successfully

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/leebrouse/MicroService-in-Go/authentication-service/data"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+)
+
+const webPort = "80"
+
+var count int64
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+
+	log.Println("Starting authentication service")
+	//TODO: connect the database
+	conn := connectToDB()
+	if conn != nil {
+		log.Panicln("Can`t connect to the Postgres")
+	}
+	//set config
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.router(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// open db
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// connect to the pg
+func connectToDB() *sql.DB {
+	//using os package to grab the dsn
+	dsn := os.Getenv("DSN")
+	//loop and the max count can`t surpass 3 times
+	for {
+		//call openDB
+		connection, err := openDB(dsn)
+		if err != nil {
+			fmt.Println("Postgres not ready yet")
+			count++
+		} else {
+			log.Println("Connect to the Postgres")
+			return connection
+		}
+
+		//surpass 10 time then break
+		if count > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds...")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,25 @@
+module github.com/leebrouse/MicroService-in-Go/authentication-service
+
+go 1.24.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/cors v1.2.1
+	golang.org/x/crypto v0.36.0
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+)
+
+require (
+	github.com/jackc/pgconn v1.14.3
+	github.com/jackc/pgx/v4 v4.18.3
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
